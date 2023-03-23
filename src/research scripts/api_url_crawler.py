baf87080e97016c3eb8f768695cf443a3c7befb1
patrcoff <90782615+patrcoff@users.noreply.github.com>
2023-03-23 17:56:14 +0000

    adds script to scrape help docs for api endpoints

---
+++ src/research scripts/api_url_crawler.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://help.ivanti.com/iv/help/en_US/isec/API/Topics/"
+
+page = requests.get(BASE_URL + "Welcome.htm")
+
+#print(page.text)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+links = [x for x in soup.find_all("a")]
+
+endpoints = []
+
+for link in links:
+    href = link.get("href")
+    if href not in ["http://www.ivanti.com/en-US/company/legal", "#",None]:
+        #print("|"+href+"|")
+        endpoints.append(href)
+output = []
+for endpoint_url in endpoints:
+    try:
+
+        url = BASE_URL + endpoint_url
+        #print(url)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        endpoint_base_url = soup.find('pre', {'class': 'prettyprint'}).text#.find('span', {'class':'com'})
+        #print(endpoint_base_url)
+        if "\n" in endpoint_base_url:
+            for el in endpoint_base_url.split():
+                output.append(el.strip())
+        else:
+            output.append(endpoint_base_url)
+    except AttributeError:
+        print("attempted item endpoint_url")
+
+with open('base_urls.txt','w+') as f:
+    for url in output:
+        print(url.strip())
+        f.write(f'{url.strip()}\n')
