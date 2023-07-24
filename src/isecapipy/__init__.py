1fd2728ae2b0072ec0c136d6c50ecf74d48a76da
patrcoff <90782615+patrcoff@users.noreply.github.com>
2023-07-24 12:26:41 +0100

    removes unused script + improves module imports

--- src/isecapipy/__init__.py
+++ src/isecapipy/__init__.py
@@ -1,3 +1,16 @@
 """Here is a placeholder package docstring"""
 
-from .api_mapping import *
+from .api_mapping import (
+    placeholder,
+    Links,
+    FrameworkVersion,
+    AgentDetail,
+    AgentDeployStatus,
+    AgentDeploymentStatus,
+    AgentPolicyTask,
+    AgentStatus,
+    AgentTaskState,
+    SuccessCode,
+    ExecutedTask,
+    QueuedTask
+)
