331397029b54a886ab8db19ac6f459bb173aead7
patrcoff <90782615+patrcoff@users.noreply.github.com>
2023-07-25 18:01:20 +0100

    adds docstrings to main

--- src/isecapipy/__main__.py
+++ src/isecapipy/__main__.py
@@ -1,12 +1,18 @@
+"""The main entry point of the packlage - for CLI usage tbc"""
+
 #  here we will create some management commands i.e. for managing env vars, entering passwords etc
 #  could also include some CLI query commands, i.e. get agent with hostname - pretty print details
 
 
 def manage():
+    """A placeholder for a management script."""
+
     print("configured correctly")
 
 
 def main():
+    """A placeholder for main entry point (for a cli)"""
+
     pass
 
 
