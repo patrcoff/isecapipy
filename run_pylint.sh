a09ca66d6a447a72afbd012798207f5edd343023
patrcoff <90782615+patrcoff@users.noreply.github.com>
2023-07-27 10:35:36 +0100

    changes to #!/usr/bin/env sh

--- run_pylint.sh
+++ run_pylint.sh
@@ -1,2 +1,9 @@
-#!/usr/bin/sh
-../.venv/bin/python -m pylint isecapipy
+#!/usr/bin/env sh
+
+if test -f "../.venv/bin/python"; then
+    ../.venv/bin/python -m pylint isecapipy
+fi
+
+if test -f "../.venv//python"; then
+    ../.venv/Scripts/python -m pylint isecapipy
+fi
--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,9 @@
-#!/usr/bin/sh
-../.venv/bin/python -m pytest tests
+#!/usr/bin/env sh
+
+if test -f "../.venv/bin/python"; then
+    ../.venv/bin/python -m pytest tests
+fi
+
+if test -f "../.venv//python"; then
+    ../.venv/Scripts/python -m pytest tests
+fi
