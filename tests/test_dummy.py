dc597182200a1026cbc7651c191cc394d3ce5897
patrcoff <90782615+patrcoff@users.noreply.github.com>
2023-07-27 21:04:49 +0100

    testing coverage

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -6,3 +6,11 @@ import isecapipy
 
 def test_placeholder():
     assert isecapipy.placeholder()
+
+
+def test_main():
+    assert isecapipy.__main__.main() == None
+
+
+def test_manage():
+    assert isecapipy.manage() == None
